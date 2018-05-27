e45b2e1ca06777362d157aa4669a5063b7713a05
José Antonio Reyes <imjaroiswebdev@gmail.com>
2018-05-27 02:35:48 -0400

    WIP: auto deploying script

--- deploy.js
+++ deploy.js
@@ -1,42 +1,29 @@
 require('dot-env')
 const ghPages = require('gh-pages')
-const gitRemoteUrl = require('git-remote-url')
 const cli = require('cli')
 const chalk = require('chalk')
 
 const ghPagesConfig = {
   branch: 'master',
   cloneDir: '.site-src-cache',
+  repo: `https://${process.env.GH_TOKEN}@github.com/imjaroiswebdev/imjaroiswebdev.github.io.git`,
   message: `[${(new Date()).toLocaleDateString('es-419', { timeZone: 'America/Santiago' })}]: Auto-generated commit for deploying site`,
   add: true
 }
 
 const deployToMaster = () => new Promise((resolve, reject) => {
-  gitRemoteUrl('./', 'origin').then(function(url) {
-    // We can't log on Travis because it would leak the GitHub token.
-    // We can't use the gh-pages silent option because it makes error messages
-    // less informative (https://github.com/mozilla/oghliner/pull/58#issuecomment-147550610).
-    ghPagesConfig.logger = function () {}
+  // We can't log on Travis because it would leak the GitHub token.
+  // We can't use the gh-pages silent option because it makes error messages
+  // less informative (https://github.com/mozilla/oghliner/pull/58#issuecomment-147550610).
+  ghPagesConfig.logger = function () {}
 
-    if ('GH_TOKEN' in process.env) {
-      const match = url.match(/^git@github.com:([^/]+)\/([^.]+)\.git$/) ||
-      url.match(/^https:\/\/github.com\/([^/]+)\/([^.]+)\.git$/)
-
-      if (match) {
-        url = 'https://' + process.env.GH_TOKEN + '@github.com/' + match[1] + '/' + match[2] + '.git'
-      }
-
-      ghPagesConfig.repo = url
+  ghPages.publish('build', ghPagesConfig, function (err) {
+    if (err) {
+      reject(err)
+    } else {
+      resolve()
     }
-
-    ghPages.publish('build', ghPagesConfig, function (err) {
-      if (err) {
-        reject(err)
-      } else {
-        resolve()
-      }
-    })
-  }, reject)
+  })
 })
 
 cli.spinner(chalk.yellow.bold('Deploying to Github Pages...'))
@@ -51,5 +38,4 @@ deployToMaster()
     cli.spinner(chalk.red.bold('✗ ') + '… An error ocurred during the deployment process!\n', true)
 
     console.error(err)
-    process.exit(1)
   })
